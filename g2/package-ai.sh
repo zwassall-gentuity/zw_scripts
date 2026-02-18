1ab6b4722aca5597f8ef73395bff55384f86d8d4
Zachary Wassall <zwassall@gentuity.com>
2026-02-18 13:38:54 -0500

    Don't include absolute path to AI package file in checksum file

--- g2/package-ai.sh
+++ g2/package-ai.sh
@@ -8,9 +8,12 @@ fi
 
 # Identify the output files.
 version=$(< version.json jq -r .version)
-checksums_file=$(realpath .sha256sum.txt)
-package_file=$(realpath ../GenshiAI-$version.zip)
-package_checksum_file=$package_file.sha256sum.txt
+checksums_filename=.sha256sum.txt
+checksums_file=$(realpath $checksums_filename)
+package_filename=GenshiAI-$version.zip
+package_file=$(realpath ../$package_filename)
+package_checksum_filename=$package_filename.sha256sum.txt
+package_checksum_file=$(realpath ../$package_checksum_filename)
 
 # Confirm the operation with the user.
 echo "This will remove and then create the following files:
@@ -39,19 +42,21 @@ sha256sum $files | tee $checksums_file || {
     exit 1
 }
 
+pushd .. > /dev/null
+
 # Create the package file.
 echo
-pushd .. > /dev/null
 7z a -mx9 $package_file ai || {
 	popd > /dev/null
 	echo "Failed to create $package_file."
 	exit 1
 }
-popd > /dev/null
 
 # Create the package checksum file.
 echo
-sha256sum $package_file | tee $package_checksum_file || {
+sha256sum $package_filename | tee $package_checksum_file || {
     echo "Failed to create $package_checksum_file."
     exit 1
 }
+
+popd > /dev/null
